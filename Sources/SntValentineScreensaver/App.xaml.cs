3baec1fbc38ca89503c4f13e44a20ca49e54f8c8
i.suhih <i.suhih@di-group.biz>
2018-02-13 02:05:55 +0700

    init

---
+++ Sources/SntValentineScreensaver/App.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Windows.Media;
+using Application = System.Windows.Application;
+
+namespace SntValentineScreensaver
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private HwndSource winWPFContent;
+
+        private void ApplicationStartup(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length == 0 || e.Args[0].ToLower().StartsWith("/s"))
+            {
+                foreach (var s in Screen.AllScreens)
+                {
+                    if (Equals(s, Screen.PrimaryScreen))
+                    {
+                        var window = new MainWindow
+                        {
+                            Left = s.WorkingArea.Left,
+                            Top = s.WorkingArea.Top,
+                            Width = s.WorkingArea.Width,
+                            Height = s.WorkingArea.Height
+                        };
+                        window.Show();
+                    }
+                    else
+                    {
+                    }
+                }
+            }
+            else if (e.Args[0].ToLower().StartsWith("/p"))
+            {
+                MainWindow window = new MainWindow();
+                Int32 previewHandle = Convert.ToInt32(e.Args[1]);
+                IntPtr pPreviewHnd = new IntPtr(previewHandle);
+                RECT lpRect = new RECT();
+                bool bGetRect = Win32API.GetClientRect(pPreviewHnd, ref lpRect);
+
+                HwndSourceParameters sourceParams = new HwndSourceParameters("sourceParams");
+
+                sourceParams.PositionX = 0;
+                sourceParams.PositionY = 0;
+                sourceParams.Height = lpRect.Bottom - lpRect.Top;
+                sourceParams.Width = lpRect.Right - lpRect.Left;
+                sourceParams.ParentWindow = pPreviewHnd;
+                sourceParams.WindowStyle = (int)(WindowStyles.WS_VISIBLE | WindowStyles.WS_CHILD | WindowStyles.WS_CLIPCHILDREN);
+
+                winWPFContent = new HwndSource(sourceParams);
+                winWPFContent.Disposed += (o, args) => window.Close();
+                winWPFContent.RootVisual = window.MainGrid;
+
+            }
+            else if (e.Args[0].ToLower().StartsWith("/c"))
+            {
+            }
+        }
+    }
+}
---
+++ Sources/SntValentineScreensaver/DesignData/TestData.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace SntValentineScreensaver.DesignData
+{
+    public class TestData:List<Image>
+    {
+    }
+}
